2b4fb8188cefd1bfd147fb28a6276a6d7fd7bb85
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-03-27 01:28:01 +0000

    New changes

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our", "an"]
+  let adj = ["incredible", "invincible", "amazing", "awesome"]
+  let noun = ["coder", "hacker", "programmer", "geek"]
+  let ext = [".com", ".net", ".co", ".arg"]
+
+    function myDomain(){
+      const newDomains = []
+
+      for (let i = 0; i < pronoun.length; i++) {
+        newDomains.push(pronoun[i])
+        for (let j = 0; j < adj.length; j++) {
+          newDomains.push(adj[j])
+          for (let k = 0; k < noun.length; k++) {
+            newDomains.push(noun[k])
+            for (let l = 0; l < ext.length; l++) {
+              newDomains.push(ext[l])
+              
+            }
+          }
+        }
+        return newDomains
+      }
+      
+      
+    }
+  const result = myDomain()
+
+  console.log(result);
 };
